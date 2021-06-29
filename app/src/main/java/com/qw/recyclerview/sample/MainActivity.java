decb283c9a22cae62f0ef403adbcbfdf286040dc
qinwei <qinwei_it@163.com>
2021-06-29 20:59:16 +0800

    上拉加载更多onScrollStateChanged实现

--- app/src/main/java/com/qw/recyclerview/sample/MainActivity.java
+++ app/src/main/java/com/qw/recyclerview/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.qw.recyclerview.sample;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.qw.recyclerview.sample.databinding.ActivityMainBinding;
@@ -18,10 +20,15 @@ import com.qw.recyclerview.sample.databinding.ActivityMainBinding;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     private ActivityMainBinding bind;
     private ArrayList<String> modules = new ArrayList<>();
     private QAdapter adapter;
+
+    private int REFRESH_IDLE = 0;
+    private int REFRESH_PULL = 1;
+    private int REFRESH_UP = 2;
+    private int refresh_state = REFRESH_IDLE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +39,18 @@ public class MainActivity extends AppCompatActivity {
         bind.mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                refresh_state = REFRESH_PULL;
+                Toast.makeText(MainActivity.this, "start 刷新", Toast.LENGTH_SHORT).show();
                 bind.mSwipeRefreshLayout.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(MainActivity.this, "onRefresh", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "end 刷新", Toast.LENGTH_SHORT).show();
                         bind.mSwipeRefreshLayout.setRefreshing(false);
+                        for (int i = 0; i < 20; i++) {
+                            modules.add("" + i);
+                        }
+                        adapter.notifyDataSetChanged();
+                        markIdle();
                     }
                 }, 3000);
             }
@@ -44,12 +58,63 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new QAdapter();
         bind.mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        bind.mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                Log.d(TAG, "newState：" + newState);
+                if (refresh_state != REFRESH_IDLE) {
+                    return;
+                }
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && checkedIsNeedLoadMore()) {
+                    refresh_state = REFRESH_PULL;
+                    Toast.makeText(MainActivity.this, "start 加载更多", Toast.LENGTH_SHORT).show();
+                    recyclerView.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            int size = modules.size();
+                            for (int i = size; i < size + 10; i++) {
+                                modules.add("" + i);
+                            }
+                            adapter.notifyDataSetChanged();
+                            Toast.makeText(MainActivity.this, "end 加载更多", Toast.LENGTH_SHORT).show();
+                            markIdle();
+                        }
+                    }, 3000);
+                }
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+
+        });
+
         bind.mRecyclerView.setAdapter(adapter);
+        modules.clear();
         for (int i = 0; i < 20; i++) {
             modules.add("" + i);
         }
         adapter.notifyDataSetChanged();
 
+    }
+
+    private void markIdle() {
+        refresh_state = REFRESH_IDLE;
+    }
+
+    private boolean checkedIsNeedLoadMore() {
+        int lastVisiblePosition = 0;
+        RecyclerView.LayoutManager layoutManager = bind.mRecyclerView.getLayoutManager();
+        if (layoutManager instanceof LinearLayoutManager) {
+            lastVisiblePosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
+        } else if (layoutManager instanceof StaggeredGridLayoutManager) {
+            StaggeredGridLayoutManager sdlm = (StaggeredGridLayoutManager) layoutManager;
+            lastVisiblePosition = sdlm.findLastCompletelyVisibleItemPositions(null)[sdlm.findLastCompletelyVisibleItemPositions(null).length - 1];
+        }
+        return adapter.getItemCount() - lastVisiblePosition <= 5;
     }
 
     private class QAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
